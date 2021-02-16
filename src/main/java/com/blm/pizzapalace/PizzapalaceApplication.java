e0ed9c408e003ad3e8dd08fba3b6dd2a16cd2536
Birhan <birhanlmusse@gmail.com>
2021-02-16 17:10:21 -0500

    second commit

---
+++ src/main/java/com/blm/pizzapalace/PizzapalaceApplication.java
@@ -0,0 +1,13 @@
+package com.blm.pizzapalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzapalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzapalaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blm/pizzapalace/PizzapalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blm.pizzapalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzapalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
